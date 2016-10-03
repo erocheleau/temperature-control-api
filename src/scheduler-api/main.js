599c3293150b402af6e6578bea53e19fe5a1ed40
erocheleau <etienne.rocheleau@gmail.com>
2016-10-03 15:44:14 -0400

    Added server to the scheduler api

--- src/scheduler-api/main.js
+++ src/scheduler-api/main.js
@@ -1,1 +1,12 @@
 // This is the main file for the scheduler api 
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello Dreamforce!!!\n");
+});
+
+server.listen(8000);
+
+console.log("Server running at http://127.0.0.1:8000/");
